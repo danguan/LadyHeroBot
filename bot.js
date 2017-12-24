e56bcc81a7b7eca13e9287c5249df7f1e828dda0
Daniel Guan <xxcradx@gmail.com>
2017-12-24 13:48:41 -0500

    Initial image bot capabilities

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var auth = require('./auth.json')
+var images = require('./assets/images.json')
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+client.on('message', message => {
+    var prefix = "sticker!";
+    var msg = message.content;
+
+    if (msg.content.substring(0, 8) === prefix) {
+        var suffix = msg.content.substring(8);
+
+        if (images.hasOwnProperty(suffix)) {
+            msg = new Discord.Attachment(images[suffix]);
+        }
     }
+    message.channel.send(msg);
 });
 
 client.login(auth.token);
