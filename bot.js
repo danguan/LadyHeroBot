fb0ce89a94e2a360ac3e999698a2415e44f47b1f
Daniel Guan <xxcradx@gmail.com>
2017-12-24 14:21:42 -0500

    Changed pathing, added helpMsg

--- bot.js
+++ bot.js
@@ -15,7 +15,21 @@ client.on('message', message => {
         var suffix = msg.substring(8);
 
         if (images.hasOwnProperty(suffix)) {
-            message.channel.send(new Discord.Attachment(images[suffix]));
+            var stickerPath = './assets/' + images[suffix];
+            var sticker = new Discord.Attachment(stickerPath);
+
+            message.channel.send(sticker);
+        } else if (suffix === "help") {
+            var helpMsg = "sticker![sticker]  →  Posts a sticker to the channel\n" + 
+            "Type sticker!help to get a list of all available stickers\n" +
+            "Available stickers:\n";
+
+            for (var stickerName in images) {
+                helpMsg += stickerName + ", ";
+            }
+            helpMsg = helpMsg.substring(0, helpMsg.length() - 2);
+
+            message.channel.send(helpMsg);
         }
     }
 });
