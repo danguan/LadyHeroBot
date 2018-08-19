09fbd346a4f896c504ae9ac3397fbfcfff235e2e
Dan Guan <daniel.guan.bca@gmail.com>
2018-08-19 17:28:15 -0400

    fix(Response): Fix multi-response bug
    Fixed behavior where bot would spam responses after any trigger was registered

--- bot.js
+++ bot.js
@@ -1,39 +1,41 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var auth = require('./auth.json')
-var images = require('./assets/images.json')
+var auth = require('./auth.json');
+var images = require('./assets/images.json');
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    var prefix = "sticker!";
+  if (!message.author.equals(client.user)) {
+
+    var prefix = 'sticker!';
     var msg = message.content;
-
+  
     if (msg.substring(0, 8).toLowerCase() === prefix) {
-        var suffix = msg.substring(8);
-
-        if (images.hasOwnProperty(suffix)) {
-            var stickerPath = './assets/' + images[suffix];
-            var sticker = new Discord.Attachment(stickerPath);
-
-            message.channel.send(sticker);
-        } else if (suffix === "help") {
-            var helpMsg = "sticker![sticker]  →  Posts a sticker to the channel\n\n" + 
-            "Type sticker!help to get a list of all available stickers\n\n" +
-            "Available stickers:\n";
-
-            for (var stickerName in images) {
-                helpMsg += stickerName + ", ";
-            }
-            helpMsg = helpMsg.substring(0, helpMsg.length - 2);
-
-            message.channel.send(helpMsg);
-        } else {
-            message.channel.send(msg);
+      var suffix = msg.substring(8);
+  
+      if (images.hasOwnProperty(suffix)) {
+        var stickerPath = './assets/' + images[suffix];
+        var sticker = new Discord.Attachment(stickerPath);
+  
+        message.channel.send(sticker);
+      } else if (suffix === 'help') {
+        var helpMsg =
+          'sticker![sticker]  →  Posts a sticker to the channel\n\n' +
+          'Type sticker!help to get a list of all available stickers\n\n' +
+          'Available stickers:\n';
+  
+        for (var stickerName in images) {
+          helpMsg += stickerName + ', ';
         }
+        helpMsg = helpMsg.substring(0, helpMsg.length - 2);
+  
+        message.channel.send(helpMsg);
+      }
     }
+  }
 });
 
 client.login(auth.token);
