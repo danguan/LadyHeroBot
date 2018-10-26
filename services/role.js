4aea75c08f80ead6ca53141f57ad3b2fbd94ee75
Daniel Guan <daniel.guan.bca@gmail.com>
2018-10-25 22:30:47 -0400

    Refactor role method, add new roles
    - Extracted role assign functionality into reusable method
    - Add roles for colors and jobs

--- services/role.js
+++ services/role.js
@@ -3,57 +3,57 @@ const roles = require('./roles.json');
 function handleRole(suffix, member, availableRoles) {
   return new Promise((res, rej) => {
     if (roles['age'].hasOwnProperty(suffix)) {
-      const ageRole = roles['age'][suffix];
-      const roleId = availableRoles.find('name', ageRole);
-
-      for (const role in roles['age']) {
-        const currRoleId = availableRoles.find('name', roles['age'][role]).id;
-        if (member.roles.has(currRoleId)) {
-          member.removeRole(currRoleId);
-        }
-      }
-
-      member.addRole(roleId);
-
-      res(`Role ${ageRole} added to ${member.displayName}`);
+      handleAssignRole('age', suffix, member, availableRoles, res);
     } else if (roles['gender'].hasOwnProperty(suffix)) {
-      const genderRole = roles['gender'][suffix];
-      const roleId = availableRoles.find('name', genderRole);
-      for (const role in roles['gender']) {
-        const currRoleId = availableRoles.find('name', roles['gender'][role])
-          .id;
-        if (member.roles.has(currRoleId)) {
-          member.removeRole(currRoleId);
-        }
-      }
-
-      member.addRole(roleId);
-
-      res(`Role ${genderRole} added to ${member.displayName}`);
+      handleAssignRole('gender', suffix, member, availableRoles, res);
+    } else if (roles['color'].hasOwnProperty(suffix)) {
+      handleAssignRole('color', suffix, member, availableRoles, res);
+    } else if (roles['job'].hasOwnProperty(suffix)) {
+      handleAssignRole('job', suffix, member, availableRoles, res);
     } else if (suffix === 'help') {
-      let helpMsg =
-        'Role![role] or role![role]  →  Assigns the specified role to you (case insensitive)\n\n' +
-        'Type Role!help to get a list of all available roles\n\n' +
-        'Available roles:\n' +
-        'Age:\n';
-
-      for (const ageRole in roles['age']) {
-        helpMsg += ageRole + ', ';
-      }
-
-      helpMsg += '\n\n' + 'Gender:\n';
-
-      for (const genderRole in roles['gender']) {
-        helpMsg += genderRole + ', ';
-      }
-
-      helpMsg = helpMsg.substring(0, helpMsg.length - 2);
-
-      res(helpMsg);
+      handleRoleHelp(res);
     } else {
       rej();
     }
   });
+}
+
+function handleAssignRole(type, suffix, member, availableRoles, res) {
+  const typedRole = roles[type][suffix];
+  const roleId = availableRoles.find('name', typedRole);
+
+  for (const role in roles[type]) {
+    const currRoleId = availableRoles.find('name', roles[type][role]).id;
+    if (member.roles.has(currRoleId)) {
+      member.removeRole(currRoleId);
+    }
+  }
+
+  member.addRole(roleId);
+
+  res(`Role ${typedRole} added to ${member.displayName}`);
+}
+
+function handleRoleHelp(res) {
+  let helpMsg =
+    'Role![role] or role![role]  →  Assigns the specified role to you (case insensitive)\n\n' +
+    'Type Role!help to get a list of all available roles\n\n' +
+    'Available roles:\n';
+
+  for (const roleType in roles) {
+    let properRoleType = roleType.replace(/\b\w/g, l => l.toUpperCase());
+    helpMsg += properRoleType + ':\n';
+
+    for (const role in roles[roleType]) {
+      helpMsg += role + ', ';
+    }
+
+    helpMsg += '\n\n';
+  }
+
+  helpMsg = helpMsg.substring(0, helpMsg.length - 2);
+
+  res(helpMsg);
 }
 
 module.exports = {
