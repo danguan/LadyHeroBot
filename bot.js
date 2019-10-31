9d546dc9c13276c6a04a522fc9046ce7907ae8ea
Dan Guan <daniel.guan.bca@gmail.com>
2019-10-31 04:21:22 -0400

    Add initial Halloween 2019 interactions

--- bot.js
+++ bot.js
@@ -3,6 +3,13 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const { handleSticker } = require('./src/services/sticker.js');
 const { handleRole } = require('./src/services/role.js');
+const {
+  handleTreat,
+  handleAnswer,
+  handleInfo,
+  handleIntro,
+  handleFinished
+} = require('./src/services/halloween2019.js');
 const { initDb } = require('./src/database/db.js');
 const { logTo } = require('./src/services/log.js');
 
@@ -28,12 +35,12 @@ client.login(auth.token);
 // Current DM message format: "!prefix suffix"
 function handleDmMessage(message) {
   if (!message.author.equals(client.user)) {
-    const msg = message.content;
+    const msg = message.content.toLowerCase();
 
     logTo('dm.log', msg, message.author.username);
 
-    if (msg.toLowerCase().startsWith('!2019 treat')) {
-      const suffix = msg.split('!2019 treat')[1].toLowerCase();
+    if (msg.startsWith('!2019 treat')) {
+      const suffix = msg.split('!2019 treat')[1];
       handleTreat(suffix, message.author.id)
         .then(treatMsg => {
           message.channel.send(treatMsg);
@@ -42,15 +49,36 @@ function handleDmMessage(message) {
           console.error(err);
           console.error('Invalid treat specified');
         });
-    } else if (msg.toLowerCase().startsWith('!answer')) {
-      const suffix = msg.split('!answer')[1].toLowerCase();
+    } else if (msg.startsWith('!answer ')) {
+      const suffix = msg.split('!answer ')[1];
       handleAnswer(suffix, message.author.id)
         .then(answerMsg => {
           message.channel.send(answerMsg);
+
+          handleFinished(message.author.id)
+            .then(finishMsg => {
+              const candyPath = './assets/candy.png';
+              const candy = new Discord.Attachment(candyPath);
+              message.channel.send(candy);
+              message.channel.send(finishMsg);
+            })
+            .catch(err => {
+              console.error(err);
+              console.error('Not yet finished with treat');
+            });
         })
         .catch(err => {
           console.error(err);
-          console.error('Invalid role specified');
+          console.error('Invalid answer specified');
+        });
+    } else if (msg === '!info') {
+      handleInfo(message.author.id)
+        .then(dmInfoMsg => {
+          message.channel.send(dmInfoMsg);
+        })
+        .catch(err => {
+          console.error(err);
+          console.error('Error occurred while sending DM info');
         });
     }
   }
@@ -59,10 +87,10 @@ function handleDmMessage(message) {
 // Current Text message format: "prefix!suffix"
 function handleTextMessage(message) {
   if (!message.author.equals(client.user)) {
-    const msg = message.content;
+    const msg = message.content.toLowerCase();
     const member = message.member;
-    const prefix = msg.substring(0, msg.indexOf('!')).toLowerCase();
-    const suffix = msg.substring(msg.indexOf('!') + 1).toLowerCase();
+    const prefix = msg.substring(0, msg.indexOf('!'));
+    const suffix = msg.substring(msg.indexOf('!') + 1);
 
     if (prefix === 'sticker') {
       handleSticker(suffix)
@@ -81,6 +109,17 @@ function handleTextMessage(message) {
         .catch(err => {
           console.error(err);
           console.error('Invalid role specified');
+        });
+    } else if (msg === '!halloween2019') {
+      handleIntro(message.author.id)
+        .then(dmIntroMsg => {
+          message.author.send(dmIntroMsg);
+        })
+        .catch(err => {
+          console.error(err);
+          console.error(
+            'Error occurred while sending DM intro for Halloween2019'
+          );
         });
     }
   }
---
+++ src/services/halloween2019.js
@@ -0,0 +1,184 @@
+const treatsAnswers = require('./halloween2019.json');
+const {
+  getInfo,
+  createInfo,
+  updateCurrentTreat,
+  updateTreatStep
+} = require('../database/db.js');
+
+const TREAT_FINISHED = 3;
+const TOTAL_TREATS = 5;
+
+function handleTreat(suffix, userId) {
+  const currTreat = 'treat' + suffix;
+
+  if (!(currTreat in treatsAnswers['treats'])) {
+    rej('Invalid treat specified');
+  }
+
+  const currUserRow = getInfo().get({ userId });
+
+  if (!currUserRow) {
+    return handleInactive();
+  }
+
+  return new Promise((res, rej) => {
+    if (currUserRow[currTreat] === TREAT_FINISHED) {
+      const currUserScore = [
+        ['treat1 : FFBE', currUserRow['treat1']],
+        ['treat2 : FFBE EQUIP (easy)', currUserRow['treat2']],
+        ['treat3 : Final Fantasy Mascots', currUserRow['treat3']],
+        ['treat4 : Lightning and Me', currUserRow['treat4']],
+        [
+          'treat5 : Bench Universe Discord Server (very easy)',
+          currUserRow['treat5']
+        ]
+      ]
+        .filter(treatStatus => treatStatus[1] < TREAT_FINISHED)
+        .map(treat => `!2019 ${treat[0]}`);
+
+      res(
+        `Seems you've already finished that treat. Here are your available choices:\n${currUserScore.join(
+          '\n'
+        )}`
+      );
+    } else if (currUserRow['currentTreat'] !== parseInt(suffix)) {
+      updateCurrentTreat().run({ currentTreat: parseInt(suffix), userId });
+      const currUserHints = treatsAnswers['treats'][currTreat];
+      const currUserHintStr = currUserHints[currUserRow[currTreat]].join('\n');
+
+      res(
+        `Switching to treat ${suffix}. Here is your current hint:\n${currUserHintStr}`
+      );
+    } else {
+      rej('Treat specified is already current treat');
+    }
+  });
+}
+
+function handleAnswer(suffix, userId) {
+  const currUserRow = getInfo().get({ userId });
+
+  if (!currUserRow) {
+    return handleInactive();
+  }
+
+  if (currUserRow['currentTreat'] === 0) {
+    res(`Pick a treat first.`);
+  }
+
+  return new Promise((res, rej) => {
+    const currentTreat = 'treat' + parseInt(currUserRow['currentTreat']);
+    const currentTreatStep = parseInt(currUserRow[currentTreat]);
+    const currAnswers =
+      treatsAnswers['answers'][currentTreat][currentTreatStep];
+
+    if (currUserRow[currentTreat] === TREAT_FINISHED) {
+      //   const currUserScore = [
+      //     ['treat1 : FFBE', currUserRow['treat1']],
+      //     ['treat2 : FFBE EQUIP (easy)', currUserRow['treat2']],
+      //     ['treat3 : Final Fantasy Mascots', currUserRow['treat3']],
+      //     ['treat4 : Lightning and Me', currUserRow['treat4']],
+      //     [
+      //       'treat5 : Bench Universe Discord Server (very easy)',
+      //       currUserRow['treat5']
+      //     ]
+      //   ]
+      //     .filter(treatStatus => treatStatus[1] < TREAT_FINISHED)
+      //     .map(treat => `!2019 ${treat[0]}`);
+
+      //   res(
+      //     `You've already finished this treat. Choose a different treat:\n${currUserScore.join(
+      //       '\n'
+      //     )}`
+      // );
+      rej(`Already done with treat, cannot answer again`);
+    } else if (currAnswers.includes(suffix)) {
+      updateTreatStep(currentTreat).run({
+        stepValue: currentTreatStep + 1,
+        userId
+      });
+
+      const newTreatHint = treatsAnswers['treats'][currentTreat][
+        currentTreatStep + 1
+      ].join('\n');
+
+      res(newTreatHint);
+    } else {
+      rej('Wrong answer');
+    }
+  });
+}
+
+function handleInfo(userId) {
+  const currUserRow = getInfo().get({ userId });
+
+  if (!currUserRow) {
+    return handleInactive();
+  }
+
+  return new Promise(res => {
+    res(treatsAnswers['info'].join('\n'));
+  });
+}
+
+function handleIntro(userId) {
+  const currUserRow = getInfo().get({ userId });
+
+  if (!currUserRow) {
+    return new Promise(res => {
+      createInfo().run({ userId });
+      res(treatsAnswers['intro'].join('\n'));
+    });
+  }
+
+  return handleInfo(userId);
+}
+
+function handleInactive() {
+  return new Promise(res => {
+    res(
+      `Go send the message !halloween2019 in the #bot-spam channel of Lady's Discord, then we'll talk.`
+    );
+  });
+}
+
+function handleFinished(userId) {
+  const currUserRow = getInfo().get({ userId });
+  const currUserTreat = currUserRow['currentTreat'];
+  const currTreat = 'treat' + currUserTreat;
+  let done = 0;
+  const currUserTreatSteps = [
+    currUserRow['treat1'],
+    currUserRow['treat2'],
+    currUserRow['treat3'],
+    currUserRow['treat4'],
+    currUserRow['treat5']
+  ].forEach(el => {
+    if (el === TREAT_FINISHED) done++;
+  });
+
+  return new Promise((res, rej) => {
+    if (done === TOTAL_TREATS) {
+      res(
+        `I don’t know why you bothered but yay.. You did it. Woopie. Congratulations. Do I need to say more? Go ask Lady for your pat on the back.`
+      );
+    } else if (done === 1) {
+      res(
+        `Well, you managed to get a treat. Thank you for participating. Go ask Lady for your trick-or-treater role.`
+      );
+    } else if (currUserRow[currTreat] === TREAT_FINISHED) {
+      res();
+    }
+
+    rej(`Not yet done with treat.`);
+  });
+}
+
+module.exports = {
+  handleTreat,
+  handleAnswer,
+  handleInfo,
+  handleIntro,
+  handleFinished
+};
